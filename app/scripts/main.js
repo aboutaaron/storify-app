7ff96040f1f654992b5ee2dcfb0ff2abd13f8eb0
aboutaaron <aaron.colby.williams@gmail.com>
2013-04-01 15:17:04 -0700

    import data into firebase for db

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -16,12 +16,12 @@ require.config({
 
 require(['jquery', 'bootstrap', 'handlebars'], function ($) {
     'use strict';
-    var aaron = 'aboutaaron';
-    var dataRef = new Firebase('https://storify.firebaseio.com/');
+    var query = 'aboutaaron';
+    var storiesRef = new Firebase('https://storify.firebaseio.com/stories/');
 
     $.ajax({
         dataType: 'jsonp',
-        url: 'http://api.storify.com/v1/stories/search?q=' + aaron,
+        url: 'http://api.storify.com/v1/stories/search?q=' + query +'&per_page=10',
         success: function(data) {
             // Handlebars
             var source = $('#storify-template').html();
@@ -32,8 +32,26 @@ require(['jquery', 'bootstrap', 'handlebars'], function ($) {
             $.each(stories, function() {
                 $('.storify-stories').append(template(this));
 
+                // Firebase
+                storiesRef.push({
+                    user_id: this.sid,
+                    title: this.title,
+                    id: this.sid,
+                    stats: {
+                        views: this.stats.views,
+                        likes: this.stats.likes,
+                        elements: {
+                            images: this.stats.elements.image,
+                            links: this.stats.elements.link,
+                            quotes: this.stats.elements.quote,
+                            text: this.stats.elements.text,
+                            vidoes: this.stats.elements.video
+                        }
+                    }
+                });
+
                 console.log(this);
-            })
+            });
         }
     });
 });
