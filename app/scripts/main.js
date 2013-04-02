3703f0e9110d0016d1011ac9f7a49f98939a01bc
aboutaaron <aaron.colby.williams@gmail.com>
2013-04-01 18:59:46 -0700

    got firebase working via the form

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -16,41 +16,49 @@ require.config({
 
 require(['jquery', 'bootstrap', 'handlebars'], function ($) {
     'use strict';
-    var query = 'aboutaaron';
+
+    // Initialize Firebase backend
     var storiesRef = new Firebase('https://storify.firebaseio.com/stories/');
 
-    $.ajax({
-        dataType: 'jsonp',
-        url: 'http://api.storify.com/v1/stories/search?q=' + query +'&per_page=10',
-        success: function(data) {
-            // Handlebars
-            var source = $('#storify-template').html();
-            var template = Handlebars.compile(source);
+    // On the submission of a query fetch the data
+    $('#search-storify').on('click', function(e) {
+        e.preventDefault();
+        var query = $('input').val();
 
-            // Data
-            var stories = data.content.stories;
-            $.each(stories, function() {
-                // Firebase
-                storiesRef.push({
-                    title: this.title,
-                    id: this.sid,
-                    stats: {
-                        views: this.stats.views,
-                        likes: this.stats.likes,
-                        elements: {
-                            images: this.stats.elements.image,
-                            links: this.stats.elements.link,
-                            quotes: this.stats.elements.quote,
-                            text: this.stats.elements.text,
-                            vidoes: this.stats.elements.video
+        // Ajax request to Storify API
+        $.ajax({
+            dataType: 'jsonp',
+            url: 'http://api.storify.com/v1/stories/search?q=' + query +'&per_page=10',
+            success: function(data) {
+                // Handlebars
+                var source = $('#storify-template').html();
+                var template = Handlebars.compile(source);
+
+                // Iterate through the data and push the values to Firebase
+                var stories = data.content.stories;
+                $.each(stories, function() {
+                    // Firebase
+                    storiesRef.push({
+                        title: this.title,
+                        id: this.sid,
+                        stats: {
+                            views: this.stats.views,
+                            likes: this.stats.likes,
+                            elements: {
+                                images: this.stats.elements.image,
+                                links: this.stats.elements.link,
+                                quotes: this.stats.elements.quote,
+                                text: this.stats.elements.text,
+                                vidoes: this.stats.elements.video
+                            }
                         }
-                    }
-                });
+                    });
 
-                // Attach to Handlebars
-                // $('.storify-stories').append(template(this));
-                console.log(this);
-            });
-        }
-    });
-});
+                    // Attach to Handlebars
+                    // $('.storify-stories').append(template(this));
+                    console.log(this);
+                }); // .each()
+            } // success
+        }); // .ajax()
+    }); // .click()
+}); // require()
